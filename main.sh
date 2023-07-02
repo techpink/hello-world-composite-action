e4b24d49649914be23e041ac753fb6e1810c93cf
Griff <7670638+techpink@users.noreply.github.com>
2023-07-02 23:15:20 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-# clean multiline text to be passed to Github API
-function clean_multiline_text {
-  local -r input="$1"
-  local output
-  output="${input//'%'/'%25'}"
-  output="${output//$'\n'/'%0A'}"
-  output="${output//$'\r'/'%0D'}"
-  echo "${output}"
-}
-
 function main {
   local -r patcher_env_folder=${INPUT_ENV_FOLDER}
 
@@ -20,15 +10,16 @@ function main {
 
   csplit -k /tmp/patcher-output.txt -f patcher '/^manual_steps_you_must_follow/' || true
 
-  local patcher_action_updates
-  patcher_action_updates=$(clean_multiline_text "$( tail -n +2 patcher00 )")
-  echo "patcher_action_updates=${patcher_action_updates}" >> "${GITHUB_OUTPUT}"
+  EOF=$(dd if=/dev/urandom bs=15 count=1 status=none | base64)
+  echo "patcher_action_updates<<$EOF" >> "${GITHUB_OUTPUT}"
+  echo "$( tail -n +2 patcher00 )" >> "${GITHUB_OUTPUT}"
+  echo "$EOF" >> "${GITHUB_OUTPUT}"
   rm patcher00
   
   [[ -f patcher01 ]] || echo '\nNo manual steps are required\n' >> patcher01
-  local patcher_action_manual_steps
-  patcher_action_manual_steps=$(clean_multiline_text "$( tail -n +2 patcher01 )")
-  echo "patcher_action_manual_steps=${patcher_action_manual_steps}" >> "${GITHUB_OUTPUT}"
+  echo "patcher_action_manual_steps<<$EOF" >> "${GITHUB_OUTPUT}"
+  echo "$( tail -n +2 patcher01 )" >> "${GITHUB_OUTPUT}"
+  echo "$EOF" >> "${GITHUB_OUTPUT}"
   rm patcher01  
 }
 
